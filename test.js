73666a10837c6914d3412fd7d80ca851d4a301cc
David Waller <david@krawaller.se>
2018-02-18 06:31:14 +0100

    update test to use mock

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-let test = require('tape');
-
-let withPrevious = require('./index');
+const test = require('tape');
+const makeMockCallbag = require('callbag-mock');
+const withPrevious = require('./index');
 
 test('it puts previous data alongside the current', t => {
   let history = [];
@@ -17,10 +17,10 @@ test('it puts previous data alongside the current', t => {
   source.emit(2, 'error');
 
   t.deepEqual(history, [
-    ['sink', 'fromUp', 1, ['foo', undefined, true]],
-    ['sink', 'fromUp', 1, ['bar', 'foo']],
-    ['sink', 'fromUp', 1, ['baz', 'bar']],
-    ['sink', 'fromUp', 2, 'error'],
+    ['sink', 'body', 1, ['foo', undefined, true]],
+    ['sink', 'body', 1, ['bar', 'foo']],
+    ['sink', 'body', 1, ['baz', 'bar']],
+    ['sink', 'body', 2, 'error'],
   ], 'sink gets terminations and values are coupled with previous values');
 
   t.end();
@@ -39,29 +39,9 @@ test('it passes requests back up', t => {
   sink.emit(2);
 
   t.deepEqual(history, [
-    ['source', 'fromDown', 1, undefined],
-    ['source', 'fromDown', 2, undefined],
+    ['source', 'talkback', 1, undefined],
+    ['source', 'talkback', 2, undefined],
   ], 'source gets requests from sink');
 
   t.end();
 });
-
-function makeMockCallbag(name, report=()=>{}, isSource) {
-  if (report === true) {
-    isSource = true;
-    report = ()=>{};
-  }
-  let talkback;
-  let mock = (t, d) => {
-    report(name, 'fromUp', t, d);
-    if (t === 0){
-      talkback = d;
-      if (isSource) talkback(0, (st, sd) => report(name, 'fromDown', st, sd));
-    }
-  };
-  mock.emit = (t, d) => {
-    if (!talkback) throw new Error(`Can't emit from ${name} before anyone has connected`);
-    talkback(t, d);
-  };
-  return mock;
-}
